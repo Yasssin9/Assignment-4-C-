82cc22441ea4a2032e759f9ade3b2949e9a36d6e
Yasssin9 <Ahmedyassin321@gmail.com>
2024-12-18 05:18:14 +0200

    Add project files.

---
+++ Assignment 4 C#/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Assignment_4_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 13 Write a program to allow the user to enter a string and print the REVERSE of it.
+            //Console.Write("Enter string \n ");
+            //string Input = Console.ReadLine();
+
+            //string reversed = "";
+            //for (int i = Input.Length - 1; i >= 0; i--)
+            //{
+            //    reversed = reversed + Input[i];
+            //}
+
+            //Console.WriteLine($"Reversed \n {reversed}");
+            #endregion
+
+            #region 16 Write a program in C# Sharp to convert a decimal number into binary without using an array.
+            //Console.Write("Enter decimal number ");
+            //int decimalNumber = int.Parse(Console.ReadLine());
+
+            //string binary = "";
+
+            //while (decimalNumber > 0)
+            //{
+            //    binary = (decimalNumber % 2) + binary;
+            //    decimalNumber /= 2;
+            //}
+            //Console.WriteLine($"Binary number is {binary}");
+            #endregion
+
+            #region 17 Create a program that asks the user to input three points (x1, y1), (x2, y2), and (x3, y3), and determines whether these points lie on a single straight line.
+            //Console.WriteLine("Enter first point (x1, y1)");
+            //float x1 = float.Parse(Console.ReadLine());
+            //float y1 = float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter second point (x2, y2)");
+            //float x2 = float.Parse(Console.ReadLine());
+            //float y2 = float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter third point (x3, y3)");
+            //float x3 = float.Parse(Console.ReadLine());
+            //float y3 = float.Parse(Console.ReadLine());
+
+
+            //float slope1 = (y2 - y1) / (x2 - x1);
+            //float slope2 = (y3 - y2) / (x3 - x2);
+
+
+            //if (slope1 == slope2)
+            //{
+            //    Console.WriteLine("points lie on straight line");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The points are not lie on straight line");
+            //}
+            #endregion
+
+            #region 18 Within a company, the efficiency of workers is evaluated based on the duration required to complete a specific task. A worker's efficiency level is determined as follows:
+            //Console.Write("Enter the time taken ");
+            //float timeTaken = float.Parse(Console.ReadLine());
+
+            //if (timeTaken >= 2 && timeTaken <= 3)
+            //{
+            //    Console.Write("Highly Efficient");
+            //}
+            //else if (timeTaken > 3 && timeTaken <= 4)
+            //{
+            //    Console.Write("Increase Speed");
+            //}
+            //else if (timeTaken > 4 && timeTaken <= 5)
+            //{
+            //    Console.Write("Provide Training");
+            //}
+            //else if (timeTaken > 5)
+            //{
+            //    Console.Write("Required to Leave the Company");
+            //}
+            //else
+            //{
+            //    Console.Write("Invalid Input");
+            //}
+            #endregion
+
+        }
+    }
+}
